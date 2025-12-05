746f34c6bb40fb5ffb31873ba1b9db8f61732a01
tanishka <tanishkamodi43@gmail.com>
2025-12-05 17:39:11 +0530

    fix-2

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/", // Backend URL
+  baseURL: import.meta.env.VITE_API_URL, // Backend URL
   headers: {
     "Content-Type": "application/json",
   },
